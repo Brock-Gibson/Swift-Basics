f765f4cbac553a1e867fd8856f945a45b3551446
Brock Gibson <brockgibson@mwc-105137.dhcp.missouri.edu>
2018-08-28 12:34:18 -0500

    First Commit

---
+++ Swift Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Brock Gibson on 8/28/18.
+//  Copyright © 2018 Brock Gibson. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
